f62ff49581e2d0014a34ce45600ac86d99b56367
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-04-24 15:31:19 +0700

    features:contact-form

--- src/components/forms/contact-form.tsx
+++ src/components/forms/contact-form.tsx
@@ -1,61 +1,167 @@
 "use client";
+import { useForm } from "react-hook-form";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import { Label } from "../ui/label";
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
 import { Textarea } from "../ui/textarea";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
+
+const nameRegex = new RegExp(
+  /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/
+);
+
+const selectOption = [
+  "Web Development",
+  "Mobile Development",
+  "Data Science",
+  "UX Design",
+] as const;
+
+const formSchema = z.object({
+  name: z.string().min(2).regex(nameRegex, "Tên không được phép ghi số!"),
+  email: z
+    .string()
+    .min(2, { message: "Địa chỉ email không hợp lệ!" })
+    .email("Phải là địa chỉ email có thật!"),
+  phone: z
+    .string()
+    .min(10, "Số điện thoại phải có 10 chữ số!")
+    .max(10, "Số điện thoại chỉ có 10 chữ số!")
+    .regex(phoneRegex, "Số điện thoại chỉ có số!"),
+  course: z.enum(selectOption).optional(),
+  message: z.string(),
+});
+
+type ContactForm = z.infer<typeof formSchema>;
+
+interface CntForm {
+  name: string;
+  email: string;
+  phone: string;
+  select?: string;
+  message?: string;
+}
 
 export default function ContactForm() {
+  const form = useForm<ContactForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      course: undefined,
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <form className="space-y-4">
-      <div className="space-y-2">
-        <Label htmlFor="name">Full Name</Label>
-        <Input id="name" placeholder="Enter your full name" />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="email">Email</Label>
-        <Input id="email" placeholder="Enter your email" type="email" />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="phone">Phone Number</Label>
-        <Input id="phone" placeholder="Enter your phone number" />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="courses">Select Courses</Label>
-        <Select>
-          <SelectTrigger>
-            <SelectValue placeholder="Select courses" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="web-development">Web Development</SelectItem>
-              <SelectItem value="mobile-development">
-                Mobile Development
-              </SelectItem>
-              <SelectItem value="data-science">Data Science</SelectItem>
-              <SelectItem value="ux-design">UX Design</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="message">Message</Label>
-        <Textarea
-          className="min-h-[120px]"
-          id="message"
-          placeholder="Enter your message"
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your full name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
-      <Button className="w-full" type="submit">
-        Submit
-      </Button>
-    </form>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your phone" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="course"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select Courses</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select Courses" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {selectOption.map((option) => (
+                    <SelectItem key={option} value={option}>
+                      {option}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Enter your message</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Message" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
