d7392d28009fd81aac15275dcff4802b8b129ac3
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-05-02 11:17:18 +0700

    section:slogan

---
+++ src/components/slogan.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import pattern from "/public/patterns/cta-pattern.jpg";
+import leftLine from "/public/patterns/cta-pattern-left.svg";
+import rightLine from "/public/patterns/cta-pattern-right.svg";
+import { site } from "@/config/site";
+import slogan from "/public/slogan.png";
+
+export default function Slogan() {
+  return (
+    <section className="lg:py-12">
+      <div className="container">
+        <div className="relative px-6 sm:px-8 py-8 sm:py-12 overflow-hidden md:px-8 rounded-3xl bg-[#101416]">
+          <div className="relative sm:max-w-2xl mx-auto text-center max-w-lg">
+            <Image src={slogan} alt="Slogan" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
