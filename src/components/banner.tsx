d8ef569531f958d91d6f5486fcdc1df7dd4e4788
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2024-05-02 13:46:12 +0700

    update: change time for countdown

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Countdown, { zeroPad } from "react-countdown";
-import trophy from "/public/trophy-icon.svg";
-import Image from "next/image";
 
 const renderer = ({
   days,
@@ -28,25 +26,25 @@ const renderer = ({
           Thời gian tuyển sinh dự kiến sẽ kết thúc sau
         </p>
         <div className="flex items-center gap-1 font-bold">
-          <span className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
+          <div className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
             {zeroPad(days)}
             <span className="text-[8px] font-medium">ngày</span>
-          </span>
+          </div>
           <span>:</span>
-          <span className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
+          <div className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
             {zeroPad(hours)}
             <span className="text-[8px] font-medium">giờ</span>
-          </span>
+          </div>
           <span>:</span>
-          <span className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
+          <div className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
             {zeroPad(minutes)}
             <span className="text-[8px] font-medium">phút</span>
-          </span>
+          </div>
           <span>:</span>
-          <span className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
+          <div className="w-8 sm:w-10 py-1 rounded-md bg-indigo-100 text-indigo-600 flex flex-col items-center leading-none">
             {zeroPad(seconds)}
             <span className="text-[8px] font-medium">giây</span>
-          </span>
+          </div>
         </div>
       </div>
     );
@@ -57,7 +55,7 @@ export default function Banner() {
   return (
     <div className="bg-indigo-600 fixed left-0 right-0 bottom-0 z-50 sm:static">
       <div className="max-w-screen-xl mx-auto px-4 py-2 sm:py-3 text-white sm:text-center md:px-8">
-        <Countdown date={new Date(2024, 4, 3)} renderer={renderer} />
+        <Countdown date={new Date(2024, 4, 15)} renderer={renderer} />
       </div>
     </div>
   );
