d47cc93b936ceb80ba51a32f29ce939888fd3d3c
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-09-04 09:49:28 +0700

    update:cancel-register

--- src/app/ung-tuyen/page.tsx
+++ src/app/ung-tuyen/page.tsx
@@ -5,22 +5,26 @@ export default function page() {
     <section id="dang-ky" className="relative py-14 mb-14 bg-[#101416]">
       <div>
         <div className="relative z-10 container">
-          <div className="max-w-lg space-y-3 px-4 sm:mx-auto sm:text-center sm:px-0">
+          <div className="max-w-xl space-y-3 px-4 sm:mx-auto sm:text-center sm:px-0">
             <h2 className="text-white text-3xl font-semibold sm:text-4xl">
-              Ứng tuyển K-tech College 2024
+              Thời gian ứng tuyển đã kết thúc.
+            </h2>
+
+            <h2 className="text-white text-3xl font-semibold sm:text-4xl">
+              Xin cám ơn bạn đã quan tâm đến chương trình.
             </h2>
           </div>
-          <div className="mt-12 mx-auto px-4 p-8 bg-white sm:max-w-lg sm:px-8 rounded-xl">
+          {/* <div className="mt-12 mx-auto px-4 p-8 bg-white sm:max-w-lg sm:px-8 rounded-xl">
             <RegisterForm />
-          </div>
+          </div> */}
         </div>
-        <div
+        {/* <div
           className="absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]"
           style={{
             background:
               "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)",
           }}
-        ></div>
+        ></div> */}
       </div>
     </section>
   );
